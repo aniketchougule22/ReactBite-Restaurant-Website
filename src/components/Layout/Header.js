cf60d330fa3c844259fd55f6a1e009209deb33ae
aniketchougule22 <chouguleaniket03@gmail.com>
2023-09-27 13:10:27 +0530

    complete the navigation menu for mobile devices

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,45 +1,118 @@
-import React from 'react'
-import { AppBar, Box, Toolbar, Typography } from '@mui/material';
-import FastfoodIcon from '@mui/icons-material/Fastfood';
-import { Link } from 'react-router-dom';
-import '../../styles/HeaderStyle.css'
+import React, { useState } from "react";
+import {
+  AppBar,
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import FastfoodIcon from "@mui/icons-material/Fastfood";
+import { Link } from "react-router-dom";
+import MenuIcon from "@mui/icons-material/Menu";
+import "../../styles/HeaderStyle.css";
 
 const Header = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  // handle drawer
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  // menu drawer
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography
+        color={"goldenrod"}
+        variant="h6"
+        component="div"
+        sx={{ flexGrow: 1, my: 2 }}
+      >
+        <FastfoodIcon />&nbsp;
+        ReactBite Restaurant
+      </Typography>
+      <Divider />
+      <ul className="mobile-navigation">
+        <li>
+          <Link to={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link to={"/menu"}>Menu</Link>
+        </li>
+        <li>
+          <Link to={"/about"}>About</Link>
+        </li>
+        <li>
+          <Link to={"/contact"}>Contact</Link>
+        </li>
+      </ul>
+    </Box>
+  );
+
   return (
     <>
       <Box>
-        <AppBar component={"nav"} sx={{bgcolor:"black"}}>
-            <Toolbar>
-                <Typography
-                    color={"goldenrod"}
-                    variant='h6'
-                    component="div"
-                    sx={{ flexGrow: 1 }}
-                >
-                    <FastfoodIcon/>
-                    ReactBite Restaurant
-                </Typography>
-                <Box sx={{ display: { xs: "none", sm: "block" } }}>
-                    <ul className='navigation-menu'>
-                        <li>
-                            <Link to={'/'}>Home</Link>
-                        </li>
-                        <li>
-                            <Link to={'/menu'}>Menu</Link>
-                        </li>
-                        <li>
-                            <Link to={'/about'}>About</Link>
-                        </li>
-                        <li>
-                            <Link to={'/contact'}>Contact</Link>
-                        </li>
-                    </ul>
-                </Box>
-            </Toolbar>
+        <AppBar component={"nav"} sx={{ bgcolor: "black" }}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              sx={{ mr: 2, display: { sm: "none" } }}
+              onClick={handleDrawerToggle}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              color={"goldenrod"}
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1 }}
+            >
+              <FastfoodIcon />
+              ReactBite Restaurant
+            </Typography>
+            <Box sx={{ display: { xs: "none", sm: "block" } }}>
+              <ul className="navigation-menu">
+                <li>
+                  <Link to={"/"}>Home</Link>
+                </li>
+                <li>
+                  <Link to={"/menu"}>Menu</Link>
+                </li>
+                <li>
+                  <Link to={"/about"}>About</Link>
+                </li>
+                <li>
+                  <Link to={"/contact"}>Contact</Link>
+                </li>
+              </ul>
+            </Box>
+          </Toolbar>
         </AppBar>
+        <Box component="nav">
+          <Drawer
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            sx={{ display: { xs: 'block', sm: 'none' },
+            "& .MuiDrwaer-paper": {
+                width: '240px',
+                boxSizing: 'border-box'
+            }
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Box>
+        <Box sx={{ p: 1 }}>
+            <Toolbar />
+        </Box>
       </Box>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
