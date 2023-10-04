a01eb1f8c3c8d368d707eddcf8c1e3fdeb574786
aniketchougule22 <chouguleaniket03@gmail.com>
2023-10-04 12:44:15 +0530

    added Menu List for Menu page

---
+++ src/data/data.js
@@ -0,0 +1,50 @@
+import Dosa from "../images/dosa.jpg";
+import Chhola from "../images/chhola.jpg";
+import Idli from "../images/idli.jpg";
+import MasalaDosa from "../images/masala.jpg";
+import Paneer from "../images/paneer.jpg";
+import Gujrati from "../images/gujrati.jpeg";
+
+const MenuList = [
+    {
+        name: "Dosa",
+        description: "A healthy and wholesome dosa preparation with the goodness of Ragi",
+        image: Dosa,
+        price: 150
+    },
+
+    {
+        name: "Idli",
+        description: "Rava Idli instant mix is made with Naturally grown ingredients. A number of ingredients like, rava, chana dal, mustard seeds together create a scrumpcious batter for soft and fluffy idlis.",
+        image: Idli,
+        price: 75
+    },
+
+    {
+        name: "Masala Dosa",
+        description: "Our Masala dosa or masale dose or masaldosa is a variation of the popular South Indian food dosa, which has its origins in Tuluva Mangalorean cuisine",
+        image: MasalaDosa,
+        price: 180
+    },
+
+    {
+        name: "Kaju Paneer",
+        description: "Our most popular variant, this mildly spiced snack is a hot favorite of paneer tikka lovers! A great source of nutrients, cashews are packed with proteins and antioxidants.",
+        image: Paneer,
+        price: 250
+    },
+
+    {
+        name: "Gujrati Thali",
+        description: "Gujarati cuisine is the cuisine of the Indian state of Gujarat. The typical Gujarati thali consists of rotli, dal or curry, rice, and shaak (a dish made up of several different combinations of vegetables and spices, which may be either spicy or sweet).",
+        image: Gujrati,
+        price: 350
+    },
+
+    {
+        name: "Chhola",
+        description: "Chhola curry sabji is made with chickpeas, tomatoes, onions, spices like cumin, coriander, turmeric, and garam masala, along with garlic, ginger, and green chilies for flavor. Garnished with cilantro, it's a flavorful vegetarian dish.",
+        image: Chhola,
+        price: 200
+    }
+]
